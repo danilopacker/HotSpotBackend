a80282cb738efbbe43ba4a7c9d93f03c4c31afe0
danilopacker-pmjs <id10830@jaraguadosul.sc.gov.br>
2023-05-12 16:52:50 -0300

    Ajustes na PMJS

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -6,6 +6,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS access(
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL,
-  email VARCHAR
-  date VARCHAR
+  email VARCHAR,
+  datetime VARCHAR
   );
